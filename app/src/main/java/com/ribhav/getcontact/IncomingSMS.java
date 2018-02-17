e2841fade70dd65e739d05ac1b37007ba95cfdd4
ribhav-pahuja <pahujaribhav984@gmail.com>
2018-02-17 20:01:39 +0530

    Changed broadcast receiver and made it compatible with Password

--- app/src/main/java/com/ribhav/getcontact/IncomingSMS.java
+++ app/src/main/java/com/ribhav/getcontact/IncomingSMS.java
@@ -3,6 +3,7 @@ package com.ribhav.getcontact;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
@@ -15,35 +16,36 @@ import android.widget.Toast;
 
 public class IncomingSMS extends BroadcastReceiver {
     final SmsManager sms = SmsManager.getDefault();
+    String password;
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context,"The message was received.",Toast.LENGTH_SHORT).show();
         Log.d("onReceive", "onReceive: Received a message.w");
         final Bundle bundle = intent.getExtras();
-
+        SharedPreferences sharedPreferences = context.getSharedPreferences("myPrefs",Context.MODE_PRIVATE);
+        password = sharedPreferences.getString("Password", null);
         try {
-
             if (bundle != null) {
-
                 final Object[] pdusObj = (Object[]) bundle.get("pdus");
-
                 for (int i = 0; i < pdusObj.length; i++) {
-
                     SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
                     String phoneNumber = currentMessage.getDisplayOriginatingAddress();
-
                     String senderNum = phoneNumber;
                     String message = currentMessage.getDisplayMessageBody();
-
-                    Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
-
-
-                    // Show Alert
-                    int duration = Toast.LENGTH_LONG;
-                    Toast toast = Toast.makeText(context,
-                            "senderNum: "+ senderNum + ", message: " + message, duration);
-                    toast.show();
-
+                    Log.d("Onreceive",password);
+                    Log.d("Onreceive",message);
+                    if(message.contains(password)){
+//                        Toast.makeText(context,"Your message seems to be compatible with the app.",Toast.LENGTH_SHORT).show();
+                        //Send back a text message containing the information of the contact.
+                        Log.d("OnReceive", "onReceive: Compatible");
+                    }else{
+                        Log.d("OnReceive", "onReceive: Incompatible");
+                    }
+//                    Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
+//                    int duration = Toast.LENGTH_LONG;
+//                    Toast toast = Toast.makeText(context,
+//                            "senderNum: "+ senderNum + ", message: " + message, duration);
+//                    toast.show();
                 } // end for loop
             } // bundle is null
 
--- app/src/main/java/com/ribhav/getcontact/MainActivity.java
+++ app/src/main/java/com/ribhav/getcontact/MainActivity.java
@@ -1,23 +1,43 @@
 package com.ribhav.getcontact;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    EditText password;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        password = findViewById(R.id.ET);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pass = password.getText().toString();
+                SharedPreferences sharedPref = MainActivity.this.getSharedPreferences("myPrefs",Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("Password", pass);
+                editor.apply();
+                Toast.makeText(getApplicationContext(),"Your password now is :- " + pass, Toast.LENGTH_SHORT).show();
+            }
+        });
         checkAndRequestPermissions();
-
     }
 
     private  boolean checkAndRequestPermissions() {
